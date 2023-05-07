299ddc13b1168054a8a001260e2bc2d54d82c8d5
aw3s0m3ss <123798409+aw3s0m3ss@users.noreply.github.com>
2023-05-07 18:43:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+
+
+let userName = "dima"
+
+localStorage.setItem("username",userName);
+console.log(localStorage.getItem('username'));
+
+sessionStorage.setItem('username',userName);
+console.log(sessionStorage.getItem('username'));
+
+
+function logMessage(count){
+    count
+   console.log(count)
+  
+}
+
+let count =1
+setTimeout(() => {
+    logMessage(count)
+  }, 3000);
+
+  let time=setInterval(function() {
+	console.log(count++);
+    if(count==4){
+      window.clearInterval(time)
+    }
+}, 3000);
+
+
+
+function calculateSquare(prom){
+  let newP = new Promise((resolve, reject) => {
+    let n1 = parseInt(prom)
+    console.log(typeof n1)
+    if (typeof n1 === 'number') {
+      resolve(
+        `Yes
+        it's
+        number`,
+        console.log(n1*2)
+      );
+    } else  {
+      reject('incorect input! you put here string make way put in a number mahahahah');
+    }
+    // reject if false
+  });
+  return newP
+}
+calculateSquare(prompt("enter num"))
